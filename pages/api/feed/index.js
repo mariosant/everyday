65af3d7a8934268362c809a6a65ffad148ee952c
Marios Antonoudiou <mariosant@sent.com>
2020-01-13 15:41:06 +0000

    Add fake data to ease the dev process
    Fixes mariosant/everyday#8

--- pages/api/feed/index.js
+++ pages/api/feed/index.js
@@ -1,18 +1,20 @@
 import fetch from 'isomorphic-unfetch'
 import prop from 'ramda/src/prop'
+import fakeData from './fakeData';
 
 const token = process.env.TOKEN
 const feedCategory = process.env.CATEGORY_ID
 const endpoint = 'https://cloud.feedly.com/v3/streams/contents'
 
-const getFeed = () =>
-  fetch(`${endpoint}?streamId=${feedCategory}`, {
+const getFeed = () => process.env.NODE_ENV === 'production'
+  ? fetch(`${endpoint}?streamId=${feedCategory}`, {
     headers: {
       authorization: `OAuth ${token}`
     }
   })
     .then(r => r.json())
     .then(prop('items'))
+  : fakeData;
 
 export default async (req, res) => {
   const feed = await getFeed()
