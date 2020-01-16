0c64779b9ae89b63454709eb95d62dd79bb6ac78
Marios Antonoudiou <mariosant@sent.com>
2020-01-16 07:52:24 +0000

    Add favicons

--- pages/_app.js
+++ pages/_app.js
@@ -30,6 +30,13 @@ export default class MyApp extends App {
             href="https://fonts.googleapis.com/css?family=Fira+Mono&display=swap"
             rel="stylesheet"
           ></link>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+					<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+					<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+					<link rel="manifest" href="/site.webmanifest" />
+					<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+					<meta name="msapplication-TileColor" content="#eeeeee" />
+					<meta name="theme-color" content="#eeeeee"></meta>
         </Head>
         <ThemeProvider theme={theme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
